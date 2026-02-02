9e7b41d208eb78aca339148ade99e10e86173d13
Ben Aw Yong <herbalvegetable@gmail.com>
2026-02-02 18:17:52 +0800

    classes and attributes

--- src/Player.java
+++ src/Player.java
@@ -1,5 +1,43 @@
 import java.util.*;
 
+
 public class Player{
+    private int playerID;
+    private int prestigePoints = 0;
+    private ArrayList<Token> tokens = new ArrayList<Token>();
+    private ArrayList<Card> boughtCards = new ArrayList<Card>();
+    private ArrayList<Card> reservedCards = new ArrayList<Card>();
+    private ArrayList<Noble> nobles = new ArrayList<Noble>();
+
+    public Player(int playerID){
+        this.playerID = playerID;
+    }
+
+    public int getPlayerID() {
+        return playerID;
+    }
+
+    public int getPrestigePoints() {
+        return prestigePoints;
+    }
+
+    public ArrayList<Token> getTokens() {
+        return tokens;
+    }
+
+    public ArrayList<Card> getBoughtCards() {
+        return boughtCards;
+    }
+
+    public ArrayList<Card> getReservedCards() {
+        return reservedCards;
+    }
+
+    public ArrayList<Noble> getNobles() {
+        return nobles;
+    }
+
     
+
+
 }
--- src/Token.java
+++ src/Token.java
@@ -1,5 +1,12 @@
-import java.util.*;
 
 public class Token{
+    private String tokenType;
     
+    public Token(String tokenType){
+        this.tokenType = tokenType;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
 }
